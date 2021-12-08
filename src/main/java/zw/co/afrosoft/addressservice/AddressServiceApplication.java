8e2b2c4f0e59fd05fe1f3722477b19b9457bf2f7
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-12-08 11:29:47 +0200

    Added Eureka Server to Register the Microservices

--- src/main/java/zw/co/afrosoft/addressservice/AddressServiceApplication.java
+++ src/main/java/zw/co/afrosoft/addressservice/AddressServiceApplication.java
@@ -2,8 +2,10 @@ package zw.co.afrosoft.addressservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AddressServiceApplication {
 
     public static void main(String[] args) {
